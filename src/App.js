ce3c58cd2cfb2b89b79045dc0169b27a70433615
unknown <Administrator@Or-PC.hrhinos.local>
2018-10-25 17:30:13 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Radium,{ StyleRoot } from 'radium';
+import Person from './Person/Person';
 import './App.css';
 
+
 class App extends Component {
+
+state = {
+  persons: [
+    {id:'1',name: 'max', age: 28},
+    {id:'2',name: 'idan', age: 30},
+    {id:'3',name: 'or', age: 28}
+  ],
+  showPersons: false
+}
+nameChanedHandler = (event ,id) =>{
+  const  personIndex = this.state.persons.findIndex(p =>{
+    return p.id === id;
+  });
+
+  const person = {
+    ...this.state.persons[personIndex]
+  };
+
+  person.name = event.target.value;
+
+  const persons = [...this.state.persons];
+
+  persons[personIndex] = person;
+  this.setState({ persons: persons })
+}
+deletePersonHandler = (personIndex) => {
+//const  persons = this.state.persons.slice();
+const persons = [...this.state.persons];
+persons.splice(personIndex, 1);
+this.setState({persons: persons});
+}
+togglePersonsHandler = () =>
+{
+  this.setState({showPersons: !this.state.showPersons});
+}
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
+
+      let persons=null;
+      if(this.state.showPersons){
+        persons = (
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} 
+              key={person.id} 
+              changed={(event) => this.nameChanedHandler(event,person.id)}/>
+            })}
+         </div>
+        );
+        style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
+      }
+      let classes =[] //['red', 'bold'].join(' ');
+      if (this.state.persons.length <= 2){
+        classes.push('red');
+      }
+      if(this.state.persons.length <=1){
+        classes.push('bold');
+      }
     return (
+      <StyleRoot>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+       <h1>Hi I'm a App</h1>       
+       <p className={classes.join(' ')}> Ravendb is the best</p>
+       <button
+       style={style}
+       onClick={ this.togglePersonsHandler}>Hide Names</button>
+      {persons}
+       </div>
+       </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
